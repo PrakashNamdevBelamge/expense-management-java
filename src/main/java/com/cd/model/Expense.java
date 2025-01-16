220ad6b52fa04671661708fca267c3b3ec99ecb5
PrakashNamdevBelamge <prakashnamdev115@gmail.com>
2025-01-16 18:24:01 +0530

    added logger

--- src/main/java/com/cd/model/Expense.java
+++ src/main/java/com/cd/model/Expense.java
@@ -12,11 +12,13 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
+@ToString
 public class Expense extends BaseEntity{
 
 	@Id
